8abb65be430bb321463d599c7e85bbef90bbb931
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-01-30 03:39:58 +0500

    stripe done 339am 30 01 2025

--- updatestripe/src/app/api/create-checkout-session/route.ts
+++ updatestripe/src/app/api/create-checkout-session/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2025-01-27.acacia",
 });
 
 export async function POST(req: Request) {
